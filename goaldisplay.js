be493e6e336bbdd0249c91a0935c04c0b7556321
Erik Haeseler <erikhaeseler@Eriks-MacBook-Pro-765.local>
2026-04-26 16:26:03 -0400

    pushing sounds?

--- goaldisplay.js
+++ goaldisplay.js
@@ -1,7 +1,7 @@
 let myTeamScore = null;
 
 document.getElementById("enableSound").addEventListener("click", () => {
-    const audio = new Audio("goal.mp3");
+    const audio = new Audio("sabres_goalhorn.mp3");
     audio.play();
     audio.currentTime = 0;
     document.getElementById("enableSound").style.display = "none";
