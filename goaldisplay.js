ed1d60611c397d5239de34c8519e3050f099b218
Erik Haeseler <erikhaeseler@Eriks-MacBook-Pro-765.local>
2026-05-16 15:26:49 -0500

    pushing changes for the clock, test for the avs game tonight

--- goaldisplay.js
+++ goaldisplay.js
@@ -1,5 +1,10 @@
 let myTeamScore = null;
 let goalPending = false;
+let gameTime = 0;
+let minutesLeft = 0;
+let secondsLeft = 0;
+let displayTime = 0;
+let teamAbbrev = "COL";
 
 // document.getElementById("enableSound").addEventListener("click", () => {
 //     const audio = new Audio("sabres_goalhorn.mp3");
@@ -13,8 +18,8 @@ async function getScore() {
     const res = await fetch(`https://corsproxy.io/?url=https://api-web.nhle.com/v1/score/now&_=${Date.now()}`);
     const data = await res.json();
     return data.games.find(game =>
-        game.homeTeam.abbrev === "BUF" ||
-        game.awayTeam.abbrev === "BUF"
+        game.homeTeam.abbrev === teamAbbrev ||
+        game.awayTeam.abbrev === teamAbbrev
     );
 }
 
@@ -25,15 +30,23 @@ async function init() {
         document.getElementById("noGameView").style.display = "flex";
         return;
     }
-    const myTeam = game.homeTeam.abbrev === "BUF" ? game.homeTeam : game.awayTeam;
+    const myTeam = game.homeTeam.abbrev === teamAbbrev ? game.homeTeam : game.awayTeam;
     myTeamScore = myTeam.score;
+    getTime(game);
     updateUI(game);
+}
+
+function getTime(game) {
+    gameTime = game.clock.timeRemaining;
+    let splitted = gameTime.split(":");
+    minutesLeft = Number(splitted[0]);
+    secondsLeft = Number(splitted[1]);
 }
 
 async function checkForGoal() {
     const game = await getScore();
     if (!game) return;
-    const myTeam = game.homeTeam.abbrev === "BUF" ? game.homeTeam : game.awayTeam;
+    const myTeam = game.homeTeam.abbrev === teamAbbrev ? game.homeTeam : game.awayTeam;
 
     if (myTeam.score > myTeamScore && !goalPending) {
         goalPending = true;
@@ -55,18 +68,34 @@ async function checkForGoal() {
 }
 
 function updateUI(game) {
-    const myTeam = game.homeTeam.abbrev === "BUF" ? game.homeTeam : game.awayTeam;
-    const otherTeam = game.homeTeam.abbrev === "BUF" ? game.awayTeam : game.homeTeam;
+    const myTeam = game.homeTeam.abbrev === teamAbbrev ? game.homeTeam : game.awayTeam;
+    const otherTeam = game.homeTeam.abbrev === teamAbbrev ? game.awayTeam : game.homeTeam;
     
     switch(game.gameState) {
+        case "OFF":
         case "FUT":
         case "PRE":
             var date = new Date(game.startTimeUTC);
             var newDate = date.toLocaleTimeString();
-            document.getElementById("time").textContent = "Game starts at" + newDate;
+            document.getElementById("time").textContent = "Game starts at " + newDate;
             break;
         case "LIVE":
-            document.getElementById("time").textContent = game.clock.timeRemaining;
+            if (game.clock.running) {
+                if (secondsLeft != 0) {
+                    secondsLeft--;
+                }
+                else if (minutesLeft > 0){
+                    minutesLeft--;
+                    secondsLeft = 59;
+                }
+            }
+            else {
+                console.log("actual: " + game.clock.timeRemaining);
+                console.log("expected: " + displayTime);
+                getTime(game);
+            }
+            displayTime = minutesLeft.toString().padStart(2, "0") + ":" + secondsLeft.toString().padStart(2, "0");
+            document.getElementById("time").textContent = displayTime;
             break;
         case "FIN":
             document.getElementById("time").textContent = "Final Score"
@@ -77,10 +106,8 @@ function updateUI(game) {
     document.getElementById("noGameView").style.display = "none";
     document.getElementById("gameView").style.display = "flex";
     document.getElementById("team1Logo").src = myTeam.logo;
-    // document.getElementById("team1Name").textContent = myTeam.name.default;
     document.getElementById("team1Score").textContent = myTeam.score;
     document.getElementById("team2Logo").src = otherTeam.logo;
-    // document.getElementById("team2Name").textContent = otherTeam.name.default;
     document.getElementById("team2Score").textContent = otherTeam.score;
 }
 
