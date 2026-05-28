872aab467f96a6b0f12e6c72feb6137039476f9d
Erik Haeseler <erikhaeseler@Eriks-MacBook-Pro-765.local>
2026-05-28 00:00:10 -0500

    pushing drink tally

--- drinkTracker/db/schema.sql
+++ drinkTracker/db/schema.sql
@@ -1,24 +1,34 @@
 DROP TABLE IF EXISTS user_table;
+DROP TABLE IF EXISTS group_table;
+DROP TABLE IF EXISTS group_users;
+DROP TABLE IF EXISTS drink_table;
+DROP TABLE IF EXISTS drink_amount;
+DROP TABLE IF EXISTS drink_tally;
 
 CREATE TABLE user_table (
     user_id SERIAL PRIMARY KEY NOT NULL,
-    username TEXT;
-)
+    username TEXT
+);
 
 CREATE TABLE group_table (
     group_id SERIAL PRIMARY KEY NOT NULL,
     group_name TEXT,
-    group_password TEXT;
-)
+    group_password TEXT
+);
 
 CREATE TABLE group_users (
     user_id INTEGER REFERENCES user_table(user_id),
-    group_id INTEGER REFERENCES group_table(group_id);
-)
+    group_id INTEGER REFERENCES group_table(group_id)
+);
 
 CREATE TABLE drink_table (
     drink_id SERIAL PRIMARY KEY NOT NULL,
     drink_name TEXT,
     drink_amount INTEGER,
-    date_added DATE;
-)
+);
+
+CREATE TABLE drink_tally (
+    user_id INTEGER REFERENCES user_table(user_id),
+    drink_id INTEGER REFERENCES drink_table(drink_id),
+    date_added DATE
+);
