7feba206ad5d7e7d6357f977f103a56c3df0a5af
Erik Haeseler <erikhaeseler@Eriks-MacBook-Pro-765.local>
2026-05-29 16:37:02 -0500

    implementing addGroup

--- drinkTracker/drinkTracker.js
+++ drinkTracker/drinkTracker.js
@@ -2,3 +2,36 @@ async function getGroups() {
     const res = await fetch('https://ehaeseler-github-io.onrender.com/get_groups');
     groups_json = res.json();
 }
+
+var modal = document.getElementById('addGroupModal');
+var modalOn = document.getElementById('modalOn');
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+  
+span.onclick = function() {
+    modal.style.display = "none";
+}
+  
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+
+
+async function addGroup() {
+    const groupName = document.getElementById('groupName');
+    const groupPass = document.getElementById('password');
+    
+    const res = await fetch('https://ehaeseler-github-io.onrender.com/add_group',
+    {
+        method: "POST", headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            name: groupName,
+            password: password
+        })
+    });
+}
