0922722c725c9c3cc9ff0a11fd5fe511ac7c6c0f
Erik Haeseler <ewhaeseler@gmail.com>
2026-07-10 01:59:25 -0500

    adding JWT

--- calorieTracker/api/server.py
+++ calorieTracker/api/server.py
@@ -2,23 +2,40 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import psycopg2
 import os
-import hashlib
-
+from pwdlib import PasswordHash
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+import jwt
+from jwt.exceptions import InvalidTokenError
+
+
 
 
 load_dotenv()
+password_hash = PasswordHash.recommended()
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # your Vite dev server origin
+    allow_origins=["http://localhost:5173"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+DUMMY_HASH = password_hash.hash("dummypassword")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 conn = psycopg2.connect(DATABASE_URL)
 
@@ -34,20 +51,102 @@ def get_conn():
         conn = psycopg2.connect(DATABASE_URL)
     return conn
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class User(BaseModel):
+    user_id: int
+    username: str
+    full_name: str
+
+
+class UserInDB(User):
+    hashed_password: str
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_user(username: str):
+    cur = get_conn().cursor()
+
+    cur.execute('''SELECT user_id, username, full_name, passw FROM user_table WHERE username = %s''', (username, ))
+    row = cur.fetchone()
+    if (row is None):
+        return None
+    return UserInDB(user_id=row[0], username=row[1], full_name=row[2], hashed_password=row[3])
+
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user:
+        verify_password(password, DUMMY_HASH)
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
 @app.get("/login")
 def check_login(username: str, password: str):
     cur = get_conn().cursor()
 
-    passw = hashlib.sha512(password.encode()).hexdigest()
-
-    cur.execute('''SELECT user_id FROM user_table WHERE username=%s AND passw=%s''', (username, passw))
-    id = cur.fetchone()
+    cur.execute('''SELECT passw FROM user_table WHERE username=%s''', (username, ))
+    hashed_pass = cur.fetchone()
+    cur.execute('''SELECT user_id FROM user_table WHERE username=%s''', (username, ))
+    uid = cur.fetchone()
     cur.close()
-    if id is None:
+    if hashed_pass is None:
         return ({"success": False})
+    password_hash.verify(password, hashed_pass[0])
 
 
-    return ({"success": True, "id": id[0]})
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return ({"success": True, "id": uid[0]})
